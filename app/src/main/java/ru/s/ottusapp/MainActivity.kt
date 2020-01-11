13706f7e121cafd459aee7aeac4060c490407226
Max <badmax77@gmail.com>
2020-01-11 23:44:27 +0300

    Initial commit

---
+++ app/src/main/java/ru/s/ottusapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package ru.s.ottusapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
