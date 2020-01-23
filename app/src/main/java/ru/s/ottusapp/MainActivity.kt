034e820e5eb8e33b9beedeab2f602b5c28bcd159
Max <badmax77@gmail.com>
2020-01-23 06:57:09 +0300

    Full layout (img,intent,color)

--- app/src/main/java/ru/s/ottusapp/MainActivity.kt
+++ app/src/main/java/ru/s/ottusapp/MainActivity.kt
@@ -1,12 +1,47 @@
 package ru.s.ottusapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
+import ru.s.ottusapp.films.Film1
+import ru.s.ottusapp.films.Film2
+import ru.s.ottusapp.films.Film3
+import ru.s.ottusapp.films.Film4
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun buttonFilm1(view: View) {
+    val intent = Intent(this@MainActivity, Film1::class.java)
+        startActivity(intent)
+        button.setTextColor(ContextCompat.getColor(this,R.color.colorAccent))
+
+    }
+
+    fun buttonFilm2(view: View) {
+        val intent = Intent(this@MainActivity, Film2::class.java)
+        startActivity(intent)
+        button2.setTextColor(ContextCompat.getColor(this, R.color.colorAccent))
+
+    }
+
+    fun buttonFilm3(view: View) {
+        val intent = Intent(this@MainActivity, Film3::class.java)
+        startActivity(intent)
+        button.setTextColor(ContextCompat.getColor(this,R.color.colorAccent))
+
+    }
+    fun buttonFilm4(view: View) {
+        val intent = Intent(this@MainActivity,Film4::class.java)
+        startActivity(intent)
+        button.setTextColor(ContextCompat.getColor(this,R.color.colorAccent))
+
     }
 }
---
+++ app/src/main/java/ru/s/ottusapp/films/Film3.kt
@@ -0,0 +1,13 @@
+package ru.s.ottusapp.films
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import ru.s.ottusapp.R
+
+class Film3 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_film3)
+    }
+}
