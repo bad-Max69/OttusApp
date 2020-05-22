f9c2d65842b9a685d82d22b1afc3f3d7df9bcb5f
maxMint <badmax>
2020-05-22 23:33:33 +0300

    fragment Created without main activ

---
+++ app/src/main/java/ru/s/ottusapp/fragment/FilmListFragment.kt
@@ -0,0 +1,27 @@
+package ru.s.ottusapp.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import ru.s.ottusapp.R
+import ru.s.ottusapp.filmsRecycler.FilmAdapter
+import ru.s.ottusapp.filmsRecycler.Films.listFilms
+
+class FilmListFragment:Fragment() {
+	override fun onCreateView(
+		inflater: LayoutInflater,
+		container: ViewGroup?,
+		savedInstanceState: Bundle?
+	): View? {
+		return inflater.inflate(R.layout.fragment_news_list, container, false)
+	}
+
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
+
+		view.findViewById<RecyclerView>(R.id.recyclerView).adapter = FilmAdapter(LayoutInflater.from(context), listFilms)
+	}
+}
